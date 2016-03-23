9c4af337c7b346e131d74d89e006e1b16c25e42d
er_mohit_agrawal <er.magrawal@gmail.com>
2016-03-23 11:10:04 +0530

    Chore :: main logger file added.

---
+++ log.js
@@ -0,0 +1,34 @@
+  var sys = require('sys');
+
+  const spawn = require('child_process').spawn;
+
+  var filename = process.argv[2];
+
+  if (!filename)
+    return sys.puts("Usage: node watcher.js filename");
+
+  // Look at http://nodejs.org/api.html#_child_processes for detail.
+  var tail = spawn("tail", ["-f", filename]);
+  sys.puts("start tailing");
+
+  tail.addListener("output", function (data) {
+    sys.puts(data);
+  });
+
+  // From nodejs.org/jsconf.pdf slide 56
+  var http = require("http");
+  http.createServer(function(req,res){
+    res.writeHead(200,{"Content-Type": "text/plain"});
+    tail.stdout.on("data", function (data) {
+      res.write(data);
+    });
+
+    tail.stdout.on("data", function (data) {
+      res.write(data);
+    });
+
+    tail.on('close', function(code) {
+      console.log(code);
+      res.end(code);
+    });
+  }).listen(8000);
